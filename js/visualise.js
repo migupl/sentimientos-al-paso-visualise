decf81103811deba882a7184f5bb31bee90fd1c1
Miguel Pagola <mipagola@gmail.com>
2023-07-13 10:32:55 +0200

    fix: change the GeoJSON loading strategy    Load as JavaScript file instead of JSON file

--- js/visualise.js
+++ js/visualise.js
@@ -1,22 +1,16 @@
+import { data } from '../notebooks/output//sentiments_by_district_geo.js'
+
 window.onload = _ => {
-    const fetchJSON = async () => {
-        const response = await fetch('../notebooks/output/sentiments_by_district_geo.json');
-        const data = await response.json();
-        return data
-    }
+    const map = document.querySelector('leaflet-map');
 
-    fetchJSON().then(data => {
-        const map = document.querySelector('leaflet-map');
-
-        Object.keys(data).forEach(key => {
-            const features = data[key];
-            setTimeout(() => {
-                map.dispatchEvent(new CustomEvent('x-leaflet-map-geojson-add', {
-                    detail: {
-                        geojson: features
-                    }
-                }))
-            }, 200)
-        })
+    Object.keys(data).forEach(key => {
+        const features = data[key];
+        setTimeout(() => {
+            map.dispatchEvent(new CustomEvent('x-leaflet-map-geojson-add', {
+                detail: {
+                    geojson: features
+                }
+            }))
+        }, 200)
     })
 }
