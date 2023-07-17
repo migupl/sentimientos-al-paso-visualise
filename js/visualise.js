3c276fb7071d7214422897ac0cd85c674042c43c
Miguel Pagola <mipagola@gmail.com>
2023-07-17 10:35:14 +0200

    feat: add message on loading

--- js/visualise.js
+++ js/visualise.js
@@ -3,9 +3,12 @@ import { data } from '../notebooks/output//sentiments_by_district_geo.js'
 window.onload = _ => {
     const map = document.querySelector('leaflet-map');
 
+    const loadMessage = document.getElementsByClassName('load-message')[0];
+
     Object.keys(data).forEach(key => {
         const features = data[key];
         setTimeout(() => {
+            loadMessage.textContent = `Cargando el distrito de '${key}'`
             map.dispatchEvent(new CustomEvent('x-leaflet-map-geojson-add', {
                 detail: {
                     geojson: features
@@ -13,4 +16,6 @@ window.onload = _ => {
             }))
         }, 200)
     })
+
+    loadMessage.textContent = 'Se han cargado todos los distritos'
 }
